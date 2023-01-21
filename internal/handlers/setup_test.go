6df4b7abab539e95dc0aaa9d2deaaeb6de949430
Igor Pronin <goldenoronin1999@gmail.com>
2023-01-21 15:41:03 -0800

    update test

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -47,6 +47,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(app.MailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -62,6 +68,14 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
