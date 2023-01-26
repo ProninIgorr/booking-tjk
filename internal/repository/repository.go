fc1a09ab9336f091d341be965abee67d976b6f16
Igor Pronin <goldenoronin1999@gmail.com>
2023-01-25 19:41:42 -0800

    add new methods for calendar

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -20,4 +20,11 @@ type DataBaseRepo interface {
 	AllReservations() ([]models.Reservation, error)
 	AllNewReservations() ([]models.Reservation, error)
 	GetReservationByID(id int) (models.Reservation, error)
+	UpdateReservation(u models.Reservation) error
+	DeleteReservation(id int) error
+	UpdateProcessedForReservation(id, processed int) error
+	AllRooms() ([]models.Room, error)
+	GetRestrictionsForRoomByDate(roomID int, start, end time.Time) ([]models.RoomRestriction, error)
+	InsertBlockForRoom(id int, startDate time.Time) error
+	DeleteBlockByID(id int) error
 }
