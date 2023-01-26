7bfb41a65d1ccc41578d0240ca19031374b355c6
Igor Pronin <goldenoronin1999@gmail.com>
2023-01-25 19:39:51 -0800

    add new gob register

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -53,7 +53,8 @@ func run() (*driver.DB, error) {
 	gob.Register(models.Reservation{})
 	gob.Register(models.User{})
 	gob.Register(models.Room{})
-	gob.Register(models.RoomRestriction{})
+	gob.Register(models.Restriction{})
+	gob.Register(map[string]int{})
 
 	mailChan := make(chan models.MailData)
 	app.MailChan = mailChan
